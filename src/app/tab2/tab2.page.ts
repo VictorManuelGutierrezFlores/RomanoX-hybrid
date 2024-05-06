76b44f8bd5fa2efa706a8bdc82a61eccff96c34d
VictorGutierrez_ <37949239+VictorManuelGutierrezFlores@users.noreply.github.com>
2024-05-06 01:02:50 -0600

    Funcionalidad completa
    Cambios :
    -Agrega librería ReactiveFormsModule
    - Elimina componentes innecesarios
    - Agrega funcionalidad decimal-romano en Tab 2
    - Modificacion de tabs-routing

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  formulario: FormGroup;
 
-  constructor() {}
 
+  constructor(private alertController: AlertController) {
+    this.formulario = new FormGroup({
+      number: new FormControl('', Validators.required)
+    });
+  }
+  async onSubmit() {
+    let toInt: number = Number(this.formulario.value.number);
+    let converted:string = decimalARomano(toInt);
+    const alert = await this.alertController.create({
+      header:'Número convetido',
+      message:'El número ingresado en romano es '+converted,
+      buttons:['Ok']
+    });
+
+    await alert.present();
+
+  }
+
+
+}
+
+function decimalARomano(numero: number): string {
+  const romansValues: string[] = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  const decimalValues: number[] = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+  let decimalNumber = numero;
+  let roman = ""
+
+  for (let i = 0; i < romansValues.length; i++) {
+    while (decimalNumber >= decimalValues[i]) {
+      roman += romansValues[i];
+      decimalNumber -= decimalValues[i];
+    }
+  }
+  return roman
 }

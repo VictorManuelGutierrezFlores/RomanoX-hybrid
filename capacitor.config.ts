cfec68d7eed859b0aff0cc705efb43fe7eee2f99
VictorGutierrez_ <vmgf09@gmail.com>
2024-05-05 21:23:53 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RomanoX-hybrid',
+  webDir: 'www'
+};
+
+export default config;
